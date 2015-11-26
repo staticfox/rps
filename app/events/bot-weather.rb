f225e18bc17a0a3e191abedcac95e4298caa603a
Matt Ullman <matt@airraidsirens.com>
2015-11-25 23:18:07 -0500

    weather: Ignore non-channel messages

--- app/events/bot-weather.rb
+++ app/events/bot-weather.rb
@@ -37,15 +37,13 @@ class BotWeather
   def handle_privmsg hash
     target = hash["target"]
     target = hash["from"] if hash["target"] == @client_sid
-    #@irc.privmsg @client_sid, target, "This is only a test." if hash["command"].downcase == "!test"
 
-    if hash["command"].downcase == "!weather"
+    if ["!weather", "!w"].include? hash["command"].downcase and target.include?("#")
       run(hash["parameters"]).each do |line|
         @irc.privmsg @client_sid, target, line
       end
     end
 
-    #@irc.privmsg @client_sid, "Ryan", "#{hash['from']} is an oper." if @irc.is_oper_uid hash["from"]
   end
 
   def init e, m, c, d
