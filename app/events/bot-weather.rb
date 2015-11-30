00cd929d332e6054071cc34c7f3c3ebb93425758
Matt Ullman <matt@airraidsirens.com>
2015-11-30 17:42:07 -0500

    weather: Catch exceptions as well

--- app/events/bot-weather.rb
+++ app/events/bot-weather.rb
@@ -12,22 +12,27 @@ class BotWeather
 
   def run parameters
     array = []
-    data = @w_api.conditions_for(parameters)
-    data = data["current_observation"]
+    begin
+      data = @w_api.conditions_for(parameters)
+      data = data["current_observation"]
 
-    if data.nil?
-      array.push("No data could be found.")
-      return array
+      if data.nil?
+        array.push("No data could be found.")
+        return array
+      end
+
+      location = data["observation_location"]["full"]
+      lastupdated = data["observation_time"]
+      description = data["nowcast"]
+      wind = data["wind_string"]
+      weather = data["weather"]
+      temp = data["temperature_string"]
+      humidity = data["relative_humidity"]
+      dewpoint = data["dewpoint_string"]
+    rescue StandardError => e
+      @irc.privmsg @client_sid, @config["debug-channels"]["bot"], "[ERROR] #{__method__.to_s}: #{e.message}"
+      return "An unexpected error has occured. The developers have been notified."
     end
-
-    location = data["observation_location"]["full"]
-    lastupdated = data["observation_time"]
-    description = data["nowcast"]
-    wind = data["wind_string"]
-    weather = data["weather"]
-    temp = data["temperature_string"]
-    humidity = data["relative_humidity"]
-    dewpoint = data["dewpoint_string"]
 
     array.push("#{lastupdated} - #{location} - Temp: #{temp} - Info: #{weather} - Winds: #{wind} - Humidity: #{humidity} - Dewpoint: #{dewpoint}")
     array.push(description.gsub("\n", ' ')[1..-2])
@@ -56,16 +61,16 @@ class BotWeather
 
     @w_api = Wunderground.new("d58c72fb57de4a46")
 
-    config = @c.Get
-    @parameters = config["connections"]["clients"]["irc"]["parameters"]
+    @config = @c.Get
+    @parameters = @config["connections"]["clients"]["irc"]["parameters"]
     @client_sid = "#{@parameters["sid"]}000003"
     @initialized = false
 
     @e.on_event do |type, hash|
       if type == "Bot-Chat"
         if !@initialized
-          config = @c.Get
-          @irc = IRCLib.new hash["name"], hash["sock"], config["connections"]["databases"]["test"]
+          @config = @c.Get
+          @irc = IRCLib.new hash["name"], hash["sock"], @config["connections"]["databases"]["test"]
           @initialized = true
         end
         handle_privmsg hash if hash["msgtype"] == "PRIVMSG"
