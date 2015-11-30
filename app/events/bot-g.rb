bbea3f56db5b7dec890aeedb27d2cb37269f2818
Matt Ullman <matt@airraidsirens.com>
2015-11-30 17:40:46 -0500

    google: Catch exceptions in the api and send to debug

--- app/events/bot-g.rb
+++ app/events/bot-g.rb
@@ -15,18 +15,26 @@ class BotG
   end
 
   def gc parameters
-    doc = Mechanize.new.get("http://173.194.115.72/search?q=#{url_encode(parameters)}")
-    result = doc.search("//h2[@class='r']").inner_text
+    begin
+      doc = Mechanize.new.get("http://173.194.115.72/search?q=#{url_encode(parameters)}")
+      result = doc.search("//h2[@class='r']").inner_text
+    rescue StandardError => e
+      @irc.privmsg @client_sid, @config["debug-channels"]["bot"], "[ERROR] #{__method__.to_s}: #{e.message}"
+      return "An unexpected error has occured. The developers have been notified."
+    end
     return "No Result Found." if result.empty?
     return result
   end
 
   def gs parameters
-    data = Google::Search::Web.new(:query => url_encode(parameters))
-    array = []
-
-    data.each { |result| array.push(result) }
-
+    begin
+      data = Google::Search::Web.new(:query => url_encode(parameters))
+      array = []
+      data.each { |result| array.push(result) }
+    rescue StandardError => e
+      @irc.privmsg @client_sid, @config["debug-channels"]["bot"], "[ERROR] #{__method__.to_s}: #{e.message}"
+      return "An unexpected error has occured. The developers have been notified."
+    end
     return "#{array[0].title} - #{array[0].uri}" if array[0]
     return "No Result Found."
   end
@@ -57,16 +65,16 @@ class BotG
     @c = c
     @d = d
 
-    config = @c.Get
-    @parameters = config["connections"]["clients"]["irc"]["parameters"]
+    @config = @c.Get
+    @parameters = @config["connections"]["clients"]["irc"]["parameters"]
     @client_sid = "#{@parameters["sid"]}000003"
     @initialized = false
 
     @e.on_event do |type, hash|
       if type == "Bot-Chat"
         if !@initialized
-          config = @c.Get
-          @irc = IRCLib.new hash["name"], hash["sock"], config["connections"]["databases"]["test"]
+          @config = @c.Get
+          @irc = IRCLib.new hash["name"], hash["sock"], @config["connections"]["databases"]["test"]
           @initialized = true
         end
         handle_privmsg hash if hash["msgtype"] == "PRIVMSG"
