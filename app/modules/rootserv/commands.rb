0549d824c7838f8057db8baa574df2fe82d2a5b7
Matt Ullman <matt@airraidsirens.com>
2015-12-02 18:23:45 -0500

    rootserv: tabcomplete fail

--- app/modules/rootserv/commands.rb
+++ app/modules/rootserv/commands.rb
@@ -217,7 +217,7 @@ class RootservCommands
       return @irc.notice @client_sid, target, "New nickname not specified"
     elsif params[1][0] =~ /[0-9]/
       return @irc.notice @client_sid, target, "Cannot SVSNICK starting with a number"
-    elsif parameters[1].length > 30
+    elsif params[1].length > 30
       return @irc.notice @client_sid, target, "Nicks cannot be greater than 30 characters."
     else
       targetobj = @irc.get_nick_object params[0]
